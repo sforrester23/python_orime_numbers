fe142061b56b5b05026ae7a20b3b656e9c2d7030
Sam Forrester <SForrester@spartaglobal.com>
2019-10-30 21:14:17 +0000

    added comments

--- functions.py
+++ functions.py
@@ -1,23 +1,38 @@
+# function for determining if the argument parsed number is prime or not
 def is_prime_number(number):
+    # make it an integer
     number = int(number)
+    # start the count
     index = 1
+    # make an empty list of the number's factors
     list_of_factors = []
+    # for the whole time the count is below or equal to the number
     while index <= number:
+        # if the number is divisible by the count
         if number%index == 0:
+            # add that number to the factors list, as it is a factor of the number
             list_of_factors.append(index)
+        # count up, regardless of whether it is a factor or not (hence the indent)
         index += 1
 
-
+    # if the list of factors is only 1 and the number itself, it is by definition, prime!
     if list_of_factors == [1, number]:
         return True
     else:
         return False
 
+# function for finding the highest number in a range (assuming range starts at zero, only taking an argument of the highest number)
 def highest_prime_in_range(number):
+    # make sure the argument number is an integer
     number = int(number)
+    # the highest prime to begin with is, by default, 1
     highest_prime = 1
+    # iterate over the range of values 0 to the inputted number (using index placeholder, so we need the highest to be number+1 as they count from 0, not 1)
     for index in range(1, number+1):
-        if is_prime_number(index) == True:
+        # if the number is prime, set the highest prime so far to be that
+        if is_prime_number(index):
             highest_prime = index
+        # by the end of the loop, we have checked all numbers, and therefore the highest_prime variable will hold the highest prime
+    # return it
     return highest_prime
 
--- run.py
+++ run.py
@@ -1,39 +1,44 @@
 from functions import *
 
+# give the user options
 print('What do you want to do today?')
 print('1 - Check if a number is prime.')
 print('2 - find the highest prime in a given range.')
 print('Type exit to leave.')
 
+# get the user's input
 choice_input = input("Please choose from the above options: ").strip().upper()
+# the whole time the user hasn't told us to exit
 while choice_input != "EXIT":
+    # if they choose to check if a number is prime
     if choice_input == "1":
+        # ask them for a number to check is prime
         prime_number_check_input = input("Please input a number that you wish to check is prime: ")
+        # if that number is prime
         if is_prime_number(prime_number_check_input):
+            # Tell them it's prime
             print('That number is prime!')
             print(" ")
+        # if it's not prime
         else:
+            # tell them
             print('That number is not prime...')
             print(" ")
+    # if the user chooses to check highest prime in a range
     elif choice_input == "2":
+        # ask them for the number to check
         print("I, Computebot, will find the highest number up to an including the number you input.")
         prime_number_range_input = input("Please enter the maximum number you wish to find the highest prime for: ")
+        # tell the user the highest prime number in that range
         print("The highest prime in the range 0 to {} is: {}.".format(prime_number_range_input, highest_prime_in_range(prime_number_range_input)))
+    # if they haven't selected a valid option, tell them
     else:
         print("Sorry, that's not an option! Please try again.")
 
+    # give them the options again
     print('What do you want to do today?')
     print('1 - Check if a number is prime.')
     print('2 - find the highest prime in a given range.')
     print('Type exit to leave.')
+    # ask them the options again, so the loop can start again
     choice_input = input("Please choose from the above options: ").strip().upper()
-
-
-
-# print(is_prime_number(15))
-
-# print(highest_prime_in_range(8))
-
-# user_input_prime = input('Please input a number for the range of which you\'d like to find prime numbers for: ')
-
-# all_primes_in_range(user_input_prime)
